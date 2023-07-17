a33aa087e3a255157594d646cb3091e262fa4c96
VarshneyGautam17 <96665741+VarshneyGautam17@users.noreply.github.com>
2023-07-17 12:44:49 +0530

    left panel Submit

--- src/User/MainSidebar.jsx
+++ src/User/MainSidebar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SiGooglechat } from "react-icons/si";
+import "../Styles/LandingPageSlideBar.css";
 import {
   BsFillBagFill,
   BsFillCalendar2DateFill,
   BsPeopleFill,
 } from "react-icons/bs";
 import { FaListUl, FaBook, FaUserGraduate } from "react-icons/fa";
-import { MdGrade } from "react-icons/md";
+import { MdGrade, MdHeight } from "react-icons/md";
 import { IoMdAnalytics } from "react-icons/io";
-import "../Styles/LandingPageSlideBar.css";
 
 export default function MainSidebar(props) {
   const [searchBtn, setSearchBtn] = useState(true);
@@ -18,34 +18,39 @@ export default function MainSidebar(props) {
 
   return (
     <>
-      <aside className="main-sidebar sidebar-dark-primary elevation-4 slidebarclass">
-        <div className="flex-shrink-0  bg-dark">
-          <a href="/" className="d-flex text-decoration-none bgcolor">
-            <span className="fs-6 fw-semibold pl-2 m-1">
-              <i class="fa fa-th mr-2" aria-hidden="true"></i>
-              Dashboard{" "}
-            </span>
-          </a>
-          <a href="/" className="d-flex pb-2 mb-2 text-decoration-none bgcolor">
-            <span className="fs-6 fw-semibold pl-2 m-1">
-              <i className="fa fa-cubes mr-2" aria-hidden="true"></i>
-              Marketing{" "}
-            </span>
-          </a>
+      <aside
+        className="main-sidebar sidebar-dark-primary elevation-4 "
+        style={{ position: "fixed", overflow: "scrollY", height: 1 }}
+      >
+        <Link to="index3.html" className="brand-link">
+          <i class="fa fa-th mr-2 text-white" aria-hidden="true"></i>
+          <span className="brand-text font-weight-light text-white">Dahsboard</span>
+        </Link>
+        <div className="sidebar ">
+          <div className="user-panel mt-2 mb-3 d-flex">
+              <i className="fa fa-cubes pl-1 mr-2 pt-2 pb-2 text-white" aria-hidden="true"></i>
+              <Link to="/" className="d-block fs-5 pb-2 text-white">
+                Markrting
+              </Link>
+        
+          </div>
 
-          <ul className="list-unstyled p-0  m-0">
-            <li className="mb-1">
-              <button
-                className="btn   btnclass text-white  collapsed"
-                data-bs-toggle="collapse"
-                data-bs-target="#home-collapse"
-                aria-expanded="false"
-              >
+          {/* <!-- Sidebar Menu --> */}
+          <nav className="mt-2 ">
+            <ul
+              className="nav nav-pills nav-sidebar flex-column"
+              data-widget="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+            <input type="checkbox" id="showItemsCheckbox1" />
+              <label htmlFor="showItemsCheckbox1" id="showItemsBtn1" className="p-0 m-0" >
                 <i className="fa fa-user mr-2" aria-hidden="true"></i>
                 Customer
-              </button>
-              <div className="collapse" id="home-collapse" style={{}}>
-                <ul className="bgcolor tcolor list-unstyled fw-normal  pb-1 small">
+                <span className="arrow"  />
+              </label>
+              <div id="itemContainer1">
+                <ul>
                   <li>
                     <a href="#" className="tcolor">
                       <i
@@ -66,19 +71,15 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn   btnclass text-white collapsed"
-                data-bs-toggle="collapse"
-                data-bs-target="#dashboard-collapse"
-                aria-expanded="false"
-              >
+
+              <input type="checkbox" id="showItemsCheckbox2" />
+              <label htmlFor="showItemsCheckbox2" id="showItemsBtn2">
                 <i className="fa fa-user mr-2" aria-hidden="true"></i>
                 Lead
-              </button>
-              <div className="collapse" id="dashboard-collapse" style={{}}>
-                <ul className=" bgcolor   list-unstyled fw-normal  pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer2">
+                <ul>
                   <li>
                     <a href="#" className="tcolor">
                       <i
@@ -99,19 +100,15 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn   btnclass text-white collapsed"
-                data-bs-toggle="collapse"
-                data-bs-target="#orders-collapse"
-                aria-expanded="false"
-              >
+
+              <input type="checkbox" id="showItemsCheckbox3" />
+              <label htmlFor="showItemsCheckbox3" id="showItemsBtn3">
                 <i className="fa fa-user mr-2" aria-hidden="true"></i>
                 Vendor
-              </button>
-              <div className="collapse" id="orders-collapse">
-                <ul className="  bgcolor  list-unstyled  fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer3">
+                <ul>
                   <li>
                     <a href="#" className="tcolor">
                       <i
@@ -132,19 +129,15 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn align-items-center text-white btnclass collapsed"
-                data-bs-toggle="collapse"
-                data-bs-target="#Email-collapse"
-                aria-expanded="false"
-              >
+
+              <input type="checkbox" id="showItemsCheckbox4" />
+              <label htmlFor="showItemsCheckbox4" id="showItemsBtn4">
                 <i className="fa fa-user mr-2" aria-hidden="true"></i>
                 Email
-              </button>
-              <div className="collapse" id="Email-collapse">
-                <ul className="list-unstyled bgcolor  fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer4">
+                <ul>
                   <li>
                     <a href="#" className=" tcolor rounded">
                       <i
@@ -165,35 +158,33 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
 
-            <li className="mb-1 d-flex">
-              <div className="col-5 align-items-left">
-                <button
-                  className="btn text-white p-0"
-                  style={{ fontSize: 18, border: "none" }}
-                >
-                  <i className="fa fa-tasks mr-2" aria-hidden="true"></i>
-                  Task
-                </button>
-              </div>
-              <div className="col-7 text-right p-0">
-                <button className="btn btn-primary btnclass1 p-0">open</button>
-                <button className="btn btn-danger btnclass1 ">1</button>
-              </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn   align-items-center text-white btnclass collapsed"
-                data-bs-toggle="collapse"
-                data-bs-target="#Interaction-collapse"
-                aria-expanded="false"
-              >
+              <li className="mb-1 mr-1 d-flex">
+                <div className="col-5 align-items-left">
+                  <button
+                    className="btn text-white p-0"
+                    style={{ fontSize: 18, border: "none" }}
+                  >
+                    <i className="fa fa-tasks mr-2" aria-hidden="true"></i>
+                    Task
+                  </button>
+                </div>
+                <div className="col-7 text-right p-0">
+                  <button className="btn btn-primary btnclass1 p-0">
+                    open
+                  </button>
+                  <button className="btn btn-danger btnclass1 ">1</button>
+                </div>
+              </li>
+
+              <input type="checkbox" id="showItemsCheckbox5" />
+              <label htmlFor="showItemsCheckbox5" id="showItemsBtn5">
                 <i className="fa fa-sticky-note-o mr-2" aria-hidden="true"></i>
                 Interaction
-              </button>
-              <div className="collapse" id="Interaction-collapse">
-                <ul className="  bgcolor   list-unstyled fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer5">
+                <ul>
                   <li>
                     <a href="#" className=" tcolor">
                       <i
@@ -214,36 +205,33 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
 
-            <li className="mb-1 mr-0 d-flex">
-              <div className="col-8 m-0 p-0">
-                <button
-                  className="btn text-white p-0 m-0 "
-                  style={{ fontSize: 18, border: "none" }}
-                >
-                  <i className="fa fa-file-text mr-1 ml-1"></i>
-                  Service Request
-                </button>
-              </div>
-              <div className="col-4 p-0 text-right ">
-                <button className="btn btn-primary btnclass1 p-0">open</button>
-                <button className="btn btn-danger btnclass1 ">1</button>
-              </div>
-            </li>
+              <li className="mb-1 mr-1 d-flex">
+                <div className="col-8 m-0 p-0">
+                  <button
+                    className="btn text-white p-0 m-0 "
+                    style={{ fontSize: 18, border: "none" }}
+                  >
+                    <i className="fa fa-file-text mr-1 ml-1"></i>
+                    Service Request
+                  </button>
+                </div>
+                <div className="col-4 p-0 text-right ">
+                  <button className="btn btn-primary btnclass1 p-0">
+                    open
+                  </button>
+                  <button className="btn btn-danger btnclass1 ">1</button>
+                </div>
+              </li>
 
-            <li className="mb-1">
-              <button
-                className="btn   align-items-center text-white btnclass collapsed pl-2"
-                data-bs-toggle="collapse"
-                data-bs-target="#Stock-collapse"
-                aria-expanded="false"
-              >
+              <input type="checkbox" id="showItemsCheckbox6" />
+              <label htmlFor="showItemsCheckbox6" id="showItemsBtn6">
                 <i className="fa fa-bars mr-2" aria-hidden="true"></i>
                 Stock
-              </button>
-              <div className="collapse" id="Stock-collapse">
-                <ul className="  bgcolor   list-unstyled fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer6">
+                <ul>
                   <li>
                     <a href="#" className=" tcolor">
                       <i className="fa fa-bars pl-3 m-1" aria-hidden="true"></i>
@@ -252,20 +240,15 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
 
-            <li className="mb-1">
-              <button
-                className="btn   align-items-center text-white btnclass collapsed pl-2"
-                data-bs-toggle="collapse"
-                data-bs-target="#Sale-collapse"
-                aria-expanded="false"
-              >
+              <input type="checkbox" id="showItemsCheckbox7" />
+              <label htmlFor="showItemsCheckbox7" id="showItemsBtn7">
                 <i className="fa fa-indent mr-2" aria-hidden="true"></i>
                 Sales
-              </button>
-              <div className="collapse" id="Sale-collapse">
-                <ul className="   bgcolor  list-unstyled fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer7">
+                <ul>
                   <li>
                     <a href="#" className=" tcolor">
                       <i className="fa fa-th pl-3 m-1" aria-hidden="true"></i>
@@ -316,19 +299,15 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn   align-items-center text-white btnclass collapsed pl-2"
-                data-bs-toggle="collapse"
-                data-bs-target="#purchase-collapse"
-                aria-expanded="false"
-              >
+
+              <input type="checkbox" id="showItemsCheckbox8" />
+              <label htmlFor="showItemsCheckbox8" id="showItemsBtn8">
                 <i className="fa fa-indent mr-2" aria-hidden="true"></i>
                 Purchase
-              </button>
-              <div className="collapse" id="purchase-collapse">
-                <ul className="   bgcolor  list-unstyled fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer8">
+                <ul>
                   <li>
                     <a href="#" className=" tcolor">
                       <i className="fa fa-th pl-3 m-1" aria-hidden="true"></i>
@@ -343,19 +322,15 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn   align-items-center text-white btnclass collapsed pl-2"
-                data-bs-toggle="collapse"
-                data-bs-target="#setting-collapse"
-                aria-expanded="false"
-              >
+
+              <input type="checkbox" id="showItemsCheckbox9" />
+              <label htmlFor="showItemsCheckbox9" id="showItemsBtn9">
                 <i className="fa fa-cog mr-2" aria-hidden="true"></i>
                 Setting
-              </button>
-              <div className="collapse" id="setting-collapse">
-                <ul className="list-unstyled bgcolor fw-normal pb-1 small">
+                <span className="arrow" />
+              </label>
+              <div id="itemContainer9">
+                <ul>
                   <li>
                     <a href="#" className=" tcolor">
                       <i
@@ -403,21 +378,20 @@ export default function MainSidebar(props) {
                   </li>
                 </ul>
               </div>
-            </li>
-            <li className="mb-1">
-              <button
-                className="btn   align-items-center text-white btnclass collapsed pl-2"
-                data-bs-toggle="collapse"
-                data-bs-target="#setting-collapse"
-                aria-expanded="false"
-              >
-                <i className="fa fa-times-circle-o mr-2" aria-hidden="true"></i>
-                Sign Out
-              </button>
-            </li>
-            
-            {/* <li className="border-top my-3" /> */}
-          </ul>
+
+              <li className="mb-1">
+                <button
+                  className="btn btn-toggle align-items-center text-white btnclass collapsed p-1"
+                >
+                  <i
+                    className="fa fa-times-circle-o mr-2"
+                    aria-hidden="true"
+                  ></i>
+                  Sign Out
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </aside>
     </>
